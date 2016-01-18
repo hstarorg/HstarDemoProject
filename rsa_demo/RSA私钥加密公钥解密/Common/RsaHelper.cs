2277e813cdaf61f383f0062bf49b9253264c53ea
Jay.M.Hu <hm910705@163.com>
2016-01-18 15:05:26 +0800

    提交RSA私钥加密，公钥解密演示程序

---
+++ rsa_demo/RSA私钥加密公钥解密/Common/RsaHelper.cs
@@ -0,0 +1,90 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace RSA私钥加密公钥解密.Common
+{
+    public class RsaHelper
+    {
+        #region 私有函数
+        private string EncryptString(string source, BigInteger d, BigInteger n)
+        {
+            int len = source.Length;
+            int len1 = 0;
+            if ((len % 128) == 0)
+                len1 = len / 128;
+            else
+                len1 = len / 128 + 1;
+            string temp = "";
+            for (int i = 0; i < len1; i++)
+            {
+                var blockLen = 0;
+                blockLen = len >= 128 ? 128 : len;
+                var block = source.Substring(i * 128, blockLen);
+                byte[] oText = Encoding.Default.GetBytes(block);
+                var biText = new BigInteger(oText);
+                var biEnText = biText.modPow(d, n);
+                string temp1 = biEnText.ToHexString();
+                temp += temp1;
+                len -= blockLen;
+            }
+            return temp;
+        }
+
+        private string DecryptString(string source, BigInteger e, BigInteger n)
+        {
+            int len = source.Length;
+            int len1 = 0;
+            if ((len % 256) == 0)
+                len1 = len / 256;
+            else
+                len1 = len / 256 + 1;
+            string temp = "";
+            for (int i = 0; i < len1; i++)
+            {
+                var blockLen = 0;
+                blockLen = len >= 256 ? 256 : len;
+                var block = source.Substring(i * 256, blockLen);
+                var biText = new BigInteger(block, 16);
+                var biEnText = biText.modPow(e, n);
+                string temp1 = System.Text.Encoding.Default.GetString(biEnText.getBytes());
+                temp += temp1;
+                len -= blockLen;
+            }
+            return temp;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 私钥加密（加密过程,其中biD、biN是RSACryptoServiceProvider生成的D、Modulus ）
+        /// </summary>
+        /// <param name="source">要加密的字符串</param>
+        /// <param name="privateKey">私钥</param>
+        /// <returns></returns>
+        public string Encrypt(string source, string privateKey)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+            var rsaParameters = rsa.ExportParameters(true);
+            var biN = new BigInteger(rsaParameters.Modulus);
+            var biD = new BigInteger(rsaParameters.D);
+            return EncryptString(source, biD, biN);
+        }
+
+        /// <summary>
+        ///   解密过程,其中biE、biN是RSACryptoServiceProvider生成的Exponent、Modulus 
+        /// </summary>
+        /// <param name="source">加密后的字符串（密文）</param>
+        /// <param name="publicKey">公钥</param>
+        /// <returns></returns>
+        public string Decrypt(string source, string publicKey)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+            var rsaParameters = rsa.ExportParameters(false);
+            var biN = new BigInteger(rsaParameters.Modulus);
+            var biE = new BigInteger(rsaParameters.Exponent);
+            return DecryptString(source, biE, biN);
+        }
+    }
+}
---
+++ rsa_demo/RSA私钥加密公钥解密/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RSA私钥加密公钥解密.Common;
+
+namespace RSA私钥加密公钥解密
+{
+    class Program
+    {
+       private readonly static RsaHelper RsaHelper = new RsaHelper();
+        static void Main(string[] args)
+        {
+            //私钥
+            var privateKey = @"<RSAKeyValue><Modulus>2l3VY7cAyC6V/nI/5/ysy/vays9y4EumCRDF6+0u7px5Er10n05JAaWJB4f1dPzzSjZfLFENj8cnYHkR0+3EWOZzhegWgvL9ADDuTX0mqFTUnZnEzfPOAy65mpFf9VDkPu62jjA0gsrcjWxlvZTgwnglIEjefuPnM3pCu1pJMFc=</Modulus><Exponent>AQAB</Exponent><P>/gNvoe30rY5/T6YChUyj+eV5kIjfwMZoYPDrxsG9WVk5qYXCXA19dOF13qlfEdUflIz0aFI+jU0Tc+qNqKFOTw==</P><Q>3BMHQa/ulOZ2OQy4r7waGPlyCdQWVX8CKdu7oEeDY4SKXBoX7bYD5/mKrDbwKxZ7AIB4woTyZKJYfSejdgvDeQ==</Q><DP>mVxZudqBL4gpNV11iCWNmwMBMlJxRWKFAoJy172BJMDKH5+yiQFcDYVSNOztiUazmmy/43xEZ+txIzsp6/X34Q==</DP><DQ>JVgxRgtFsAy/iQTInkGMKCrEGDjeJVntEw9Pcm5QNGX8WI6w+syFMyscRgb4kEc7TiV+2HtY6ozF+U7tpQ1v+Q==</DQ><InverseQ>8XZBDYGcQQ3z84Da0o7+cUWIieCA+GFDB7hpH8PW8UGbOmFrLxkC+WKkZEjlvLR5EDggkKfSoQ1v0oWlN7N/bQ==</InverseQ><D>    BR3lGH9lnaCMfo2Ey9XS4rtrEWIyXz5xNzh/usXbBK0BSpwf4HUVfDFkeEW0+GHOUvMv4GT3JrhURuFvYSzqyIi59tZ0USGc08saDOpzBlXiAPgVdyxyEp7nYtH00tRpISDXGa4RAHyTErmyG7d56iOwdCUEUiI/sRruL+myVdE=</D></RSAKeyValue>";
+            //公钥
+            var publicKey = @"<RSAKeyValue><Modulus>2l3VY7cAyC6V/nI/5/ysy/vays9y4EumCRDF6+0u7px5Er10n05JAaWJB4f1dPzzSjZfLFENj8cnYHkR0+3EWOZzhegWgvL9ADDuTX0mqFTUnZnEzfPOAy65mpFf9VDkPu62jjA0gsrcjWxlvZTgwnglIEjefuPnM3pCu1pJMFc=</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
+            Console.WriteLine("请输入要加密的字符串：");
+            var sourceText = Console.ReadLine();
+            var cipherText = RsaHelper.Encrypt(sourceText, privateKey);
+            Console.WriteLine("加密后的字符串是：{0}", cipherText);
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("解密之后的字符串是：{0}", RsaHelper.Decrypt(cipherText, publicKey));
+            Console.ReadKey();
+        }
+    }
+}
