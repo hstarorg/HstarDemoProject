9b1bfc566726089a80df5d63833e1d5b896f5e0c
unknown <jh3r@wcmis346.buyabs.corp>
2016-01-27 18:50:29 +0800

    Add some go demo.

---
+++ golang_demo/1_hello/src/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+func main() {
+  fmt.Println("avbc")
+  time.Sleep(3e9)
+}
---
+++ golang_demo/1_hello/start.cmd
@@ -0,0 +1,76 @@
+﻿
+@echo off
+rem 当前bat的作用
+
+echo ==================begin========================
+
+cls
+
+SET GOPATH = %CD%/../1_hello
+cd %CD%/../1_hello/src
+
+color 0a
+TITLE Go Program Management Tool
+
+CLS
+
+ECHO.
+ECHO. * Go Program Management Tool * 
+ECHO. * create by Jay at 2016-1-27 * 
+ECHO.
+
+:MENU
+ECHO.
+ECHO.
+ECHO.
+ECHO. * Please enter a number to continue* 
+
+ECHO.
+    ECHO.  [1] Build 
+    ECHO.  [2] Run
+    ECHO.  [3] Install Dependencies
+    ECHO.  [4] Exit
+ECHO.
+
+ECHO.Pelase enter:
+set /p ID=
+    IF "%id%"=="1" GOTO build
+    IF "%id%"=="2" GOTO run
+    IF "%id%"=="3" GOTO installDependencies
+    IF "%id%"=="4" EXIT
+PAUSE
+
+:build
+    call :doBuild
+    GOTO MENU
+
+:run
+    call :doRun
+    GOTO MENU
+
+:installDependencies
+    call :doInstallDependencies
+    GOTO MENU
+
+
+:doBuild
+    ECHO.
+    go build
+    ECHO. build succeed.
+    goto :eof
+
+:doRun
+    ECHO.
+    go run ./main.go
+    ECHO. run succeed.
+    goto :eof
+
+:doInstallDependencies
+    ECHO.
+
+    go get 11
+
+    ECHO. install dependencies succeed.
+    goto :eof 
+
+@echo off
