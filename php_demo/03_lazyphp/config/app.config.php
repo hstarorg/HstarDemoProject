66b744a5a5f0e34fab40abaa9aed8e3a01cd98ea
Jay.M.Hu <hm910705@163.com>
2016-06-03 22:59:39 +0800

    Add lazyphp demo.

---
+++ php_demo/03_lazyphp/config/app.config.php
@@ -0,0 +1,4 @@
+<?php
+$GLOBALS['config']['site_name'] = 'PHP with lazyPHP';
+$GLOBALS['config']['site_domain'] = 'lazyphp3.sinaapp.com';
+$GLOBALS['config']['site_icon'] = image('lazyphplogowithoutname.128.png');
