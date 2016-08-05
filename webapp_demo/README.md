6a4cc6498a70b8131e8640346b59012284dba111
Jay.M.Hu <hm910705@163.com>
2016-08-06 00:38:04 +0800

    Add web app with vue demo.

---
+++ webapp_demo/README.md
@@ -0,0 +1,5 @@
+该目录下计划包含多个webapp demo项目。
+
+当前已有：
+
+1. app1-with-vue  利用Vue来开发WebApp。
---
+++ webapp_demo/app1-with-vue/src/pages/Home.vue
@@ -0,0 +1,33 @@
+<style>
+  .fixed-header{
+    position: fixed;
+    top: 0;
+    width: 100%;
+    max-width: 640px;
+    background: gray;
+    height: 50px;
+  }
+</style>
+
+<template>
+  <header class="fixed-header">
+    ABC
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    components: {},
+    data () {
+      return {
+        test: 1
+      };
+    },
+    computed: {},
+    events: {},
+    methods: {
+
+    }
+  }
+</script>
