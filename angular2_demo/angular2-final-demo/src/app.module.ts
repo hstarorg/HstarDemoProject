8045c0a0b70d6da3d3dc128ddb55efa99bb235de
Jay.M.Hu <jay.m.hu@newegg.com>
2016-09-17 14:57:52 +0800

    可运行的angular2 - demo

---
+++ angular2_demo/angular2-final-demo/src/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './pages/app.component.ts';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+};
---
+++ angular2_demo/angular2-final-demo/src/vendor.ts
@@ -0,0 +1,14 @@
+// Polyfills
+import 'reflect-metadata';
+import 'zone.js';
+
+// Angular 2
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
