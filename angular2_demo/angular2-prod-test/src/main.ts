6847b15ea78b3eb18cfab2b2a5c70027a02472f9
Jay.M.Hu <jay.m.hu@newegg.com>
2016-10-13 15:22:28 +0800

    Angular2 minify test.

---
+++ angular2_demo/angular2-prod-test/src/main.ts
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(success => console.log(`Bootstrap success`))
+  .catch(error => console.log(error));
