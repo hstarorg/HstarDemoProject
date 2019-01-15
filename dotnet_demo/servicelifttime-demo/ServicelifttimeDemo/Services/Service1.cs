73d3802498a47bf293116202318a2e501b1d1883
Hstarorg <hm910705@163.com>
2019-01-15 17:40:52 +0800

    Update demos

---
+++ dotnet_demo/servicelifttime-demo/ServicelifttimeDemo/Services/Service1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicelifttimeDemo.Services
+{
+  public class Service1
+  {
+  }
+}
