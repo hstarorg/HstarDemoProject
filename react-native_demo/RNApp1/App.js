73c7fb84e169eb1339b7312053a4774f530a40dc
Jay.M.Hu <jay.m.hu@newegg.com>
2019-01-29 18:40:27 +0800

    Add sticky list header demo.

--- react-native_demo/RNApp1/App.js
+++ react-native_demo/RNApp1/App.js
@@ -7,44 +7,59 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class StickyListHeader extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ height: 30, backgroundColor: '#ccc', zIndex: 2 }}>
+        <Text>我才是真的Header！</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class TestView extends Component {
+  _renderListItem({ item, index }) {
+    return (
+      <View style={{ width: 375 / 3, height: 100, backgroundColor: 'orange' }}>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  }
+
+  _renderParentListItem = ({ item }) => {
+    return (
+      <View>
+        <View style={{ width: 375, height: 100, backgroundColor: 'red', marginBottom: 5, marginTop: 5 }}>
+          <Text>我是一个广告</Text>
+        </View>
+        <FlatList data={item} renderItem={this._renderListItem} numColumns={3} />
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <FlatList
+        data={[this.props.dataList]}
+        renderItem={this._renderParentListItem}
+        ListHeaderComponent={<StickyListHeader />}
+        stickyHeaderIndices={[0]}
+      />
+    );
+  }
+}
+
+export default class App extends Component {
+  dataList = new Array(100).fill(0).map((x, i) => ({ name: `X${i}`, key: `key${i}` }));
+
+  render() {
+    return (
+      <SafeAreaView style={{ backgroundColor: '#ddd' }}>
+        <TestView dataList={this.dataList} />
+      </SafeAreaView>
+    );
+  }
+}
